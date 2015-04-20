a5ff6239e8f194d03523c89013169a271aec7ad9
Murtuza <ermurtuzasaifee@gmail.com>
2015-04-20 14:34:31 +0530

    Test jenkins commit

---
+++ Jenkins-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jenkins-Test
+//
+//  Created by Murtuza Saifee on 20/04/15.
+//  Copyright (c) 2015 Blisstering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jenkins-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jenkins-Test
+//
+//  Created by Murtuza Saifee on 20/04/15.
+//  Copyright (c) 2015 Blisstering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
